13ab3844839efbae5f7edca3eace65c62704a3fb
Fab14 <fabrice.de.chaumont@gmail.com>
2012-09-11 11:00:57 +0200

    initial commit

---
+++ src/plugins/fab/icydiagnose/IcyDiagnose.java
@@ -0,0 +1,16 @@
+package plugins.fab.icydiagnose;
+
+import icy.plugin.abstract_.PluginActionable;
+
+public class IcyDiagnose extends PluginActionable {
+
+	@Override
+	public void run() {
+
+		
+
+	}
+
+	
+	
+}
